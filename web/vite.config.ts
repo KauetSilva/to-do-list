24eab7bd4d7db9e8963f714ab1c0d1bd2f6b3173
kauetsilva <kauetclecio1@gmail.com>
2025-09-15 21:17:09 -0300

    fix: adjustment in build
    refs: #main

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       // Resolver problemas de compatibilidade com date-fns
